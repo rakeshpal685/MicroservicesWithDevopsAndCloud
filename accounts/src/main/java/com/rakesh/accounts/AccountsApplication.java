58e3496fd8fd7a02c146e1c156e7bd902e3e5ffa
rakesh <rakesh.pal685@gmail.com>
2024-01-10 12:02:27 +0800

    Added swagger related changes

--- accounts/src/main/java/com/rakesh/accounts/AccountsApplication.java
+++ accounts/src/main/java/com/rakesh/accounts/AccountsApplication.java
@@ -1,13 +1,42 @@
 package com.rakesh.accounts;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+/*If we are following the standard approach and creating the packages parallel to main class then spring
+will auto scan them but if we are creating the packages in some other location then we have to specify the
+location of our classes so that spring can create bean of them, using the below ways...
+@ComponentScans({@ComponentScan("com.rakesh.accounts.controller")})
+@EnableJpaRepositories("com.rakesh.accounts.repository")
+@EntityScan("com.rakesh.accounts.model")*/
+
 /*This way we are telling to the spring boot framework. Please activate the JPAAuditing and please
 use the bean with the name auditAwareImpl to understand the current auditor.*/
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+// This is used for enabling documentation using swagger
+@OpenAPIDefinition(
+    info =
+        @Info(
+            title = "Account microservice REST API Documentation",
+            description = "Here I can give description",
+            version = "v1",
+            contact = @Contact(name = "Rakesh Pal", email = "abc@xyz.com", url = "www.abc.com"),
+            license = @License(name = "Apache 2.0", url = "www.abc.com")),
+    externalDocs =
+        @ExternalDocumentation(
+            description = "This is external documentation for further reference",
+            url = "www.springboot.com"))
 public class AccountsApplication {
 
   public static void main(String[] args) {
--- accounts/src/main/java/com/rakesh/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/rakesh/accounts/controller/AccountsController.java
@@ -4,6 +4,9 @@ import com.rakesh.accounts.constants.AccountsConstants;
 import com.rakesh.accounts.dto.CustomerDto;
 import com.rakesh.accounts.dto.ResponseDto;
 import com.rakesh.accounts.service.AccountServiceInterf;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
@@ -21,11 +24,21 @@ import org.springframework.web.bind.annotation.*;
 /*So this annotation will tell to my spring boot framework to perform validations on all the REST APIs
 that I have defined inside these AccountsController.*/
 @Validated
+// Below annotations are for swagger
+@Tag(
+    name = "CRUD REST APIs for Accounts in Rakesh Bank",
+    description =
+        "CRUD REST APIs for Accounts in Rakesh Bank to CREATE,UPDATE,DELETE, FETCH account details")
 public class AccountsController {
 
   private AccountServiceInterf accountServiceInterf;
 
   @PostMapping("/create")
+  // Below are for swagger
+  @Operation(
+      summary = "Create Account REST API",
+      description = "REST API to create a new customer in the bank")
+  @ApiResponse(responseCode = "201", description = "HTTP status CREATED")
   public ResponseEntity<ResponseDto> createAccount(@Valid @RequestBody CustomerDto customerDto) {
     accountServiceInterf.createAccount(customerDto);
     return ResponseEntity.status(HttpStatus.CREATED)
@@ -33,14 +46,32 @@ public class AccountsController {
   }
 
   @GetMapping("/fetch")
+  // Below are for swagger
+  @Operation(
+      summary = "Fetch Account REST API",
+      description = "REST API to fetch customer and account details based on the mobile number",
+      responses = {
+        @ApiResponse(responseCode = "200", description = "HTTP status OK"),
+        @ApiResponse(responseCode = "404", description = "HTTP status NOT_FOUND"),
+      })
   public ResponseEntity<CustomerDto> fetchAccountDetails(
-      @RequestParam @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number must be 10 digits")
+      //      @RequestParam
+      @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number must be 10 digits")
           String mobileNumber) {
     CustomerDto customerDto = accountServiceInterf.fetchAccount(mobileNumber);
     return ResponseEntity.status(HttpStatus.OK).body(customerDto);
   }
 
   @PutMapping("/update")
+  // Below are for swagger
+  @Operation(
+      summary = "Update Account Details REST API",
+      description = "REST API to update customer and account details based on the account number",
+      responses = {
+        @ApiResponse(responseCode = "200", description = "HTTP status OK"),
+        @ApiResponse(responseCode = "404", description = "HTTP status NOT_FOUND"),
+        @ApiResponse(responseCode = "500", description = "HTTP status Internal Server Error")
+      })
   public ResponseEntity<ResponseDto> updateAccountDetails(
       @Valid @RequestBody CustomerDto customerDto) {
     boolean isUpdated = accountServiceInterf.updateAccount(customerDto);
@@ -54,8 +85,18 @@ public class AccountsController {
   }
 
   @DeleteMapping("/delete")
+  // Below are for swagger
+  @Operation(
+      summary = "Delete Account Details REST API",
+      description = "REST API to delete customer and account details based on the account number",
+      responses = {
+        @ApiResponse(responseCode = "200", description = "HTTP status OK"),
+        @ApiResponse(responseCode = "404", description = "HTTP status NOT_FOUND"),
+        @ApiResponse(responseCode = "500", description = "HTTP status Internal Server Error")
+      })
   public ResponseEntity<ResponseDto> deleteAccountDetails(
-      @RequestParam @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number must be 10 digits")
+      //      @RequestParam
+      @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number must be 10 digits")
           String mobileNumber) {
     boolean isDeleted = accountServiceInterf.deleteAccount(mobileNumber);
     if (isDeleted) {
